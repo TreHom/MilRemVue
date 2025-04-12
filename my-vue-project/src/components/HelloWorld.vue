0414ba2bca5950a5a8bc1882d4df095339515ba7
TreHom <robintammemgi@gmail.com>
2025-04-12 17:00:50 +0300

    Getting the map to work first test

---
+++ my-vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <l-map
+    style="height: 500px; width: 100%;"
+    :zoom="13"
+    :center="[58.3776, 26.7290]"
+  >
+    <l-tile-layer
+      :url="tileUrl"
+      :attribution="attribution"
+    />
+  </l-map>
+</template>
+
+<script setup>
+import 'leaflet/dist/leaflet.css'
+import { LMap, LTileLayer} from '@vue-leaflet/vue-leaflet'
+
+
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+</script>
